638d1cc32b86ab07ee884d9dff203d9bb8af2729
Rugved Dhuri <rugveddhuri@gmail.com>
2016-03-20 10:35:55 +0530

    test

--- src/vcfSplit/VCFSplit.java
+++ src/vcfSplit/VCFSplit.java
@@ -1,24 +1,2 @@
-package vcfSplit;
-
-/**
- * Author     : Rugved Dhuri
- * Title      : VCF Split
- * Description: Split VCF File into multiple VCF files
- */
-
-import java.io.BufferedReader;
-import java.io.File;
-//import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-public class VCFSplit {
-
-	public static void main(String[] args) {
-
-		// Variables
-		File source_file;
-		File destination_folder;	
-	}
-
-}
+test
+file
